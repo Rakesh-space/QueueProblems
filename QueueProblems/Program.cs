5fd3dcd6ed5edb0ce8698b9f27464ea526f498c1
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-30 14:49:51 +0530

    Adding Welcome meassage

---
+++ QueueProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueueProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to Queue Problem..");
+        }
+    }
+}
